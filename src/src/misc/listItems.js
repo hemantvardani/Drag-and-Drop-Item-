f2412d051ffe0611e0a40e541047ceaf6535dab6
hemantvardani <hemant2vardani@gmail.com>
2024-04-12 00:35:02 +0530

    list Items introduction2

---
+++ src/src/misc/listItems.js
@@ -0,0 +1,16 @@
+export const listItems=[
+    {
+         title:'List Item 1',
+        lastUpdated:'3'
+    },
+
+    {
+         title:'List Item 2',
+        lastUpdated:'7'
+    },
+
+    {
+         title:'List Item 3',
+        lastUpdated:'6'
+    }
+]
