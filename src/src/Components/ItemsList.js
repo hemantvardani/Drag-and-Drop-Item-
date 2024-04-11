3ce6cacfa603db65630d865477d8206496548fee
hemantvardani <hemant2vardani@gmail.com>
2024-04-12 00:34:32 +0530

    list Items introduction

--- src/src/Components/ItemsList.js
+++ src/src/Components/ItemsList.js
@@ -1,12 +1,40 @@
-import Card from '@mui/material/Card';
-import { BASE_COLOR } from '../misc/colors';
+import Card from "@mui/material/Card";
+import { BASE_COLOR } from "../misc/colors";
+import { listItems } from "../misc/listItems";
 
-export function ItemsList(){
-    return (<>
-    <div style={{background:BASE_COLOR}}>
-        <div><b>Project Folder 1</b></div>
+export function ItemsList() {
+  return (
+    <>
+      <div style={{ background: BASE_COLOR }}>
+        <div>
+          <b>Project Folder 1</b>
+        </div>
 
+        {listItems.map((item) => {
+          const imgLocation = item.imgLocation;
 
-    </div>
-    </>);
+          return (
+            <div style={{ padding: 10 }}>
+              <Card>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <img
+                    src={require("./../Assests/icon1.png")}
+                    width={50}
+                    height={50}
+                  />
+                  <span>{item.title}</span>
+                </div>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
